e30d74fc330bc83f25287dc96e5b07d32c7ce59d
Luc <lisson.luc@gmail.com>
2026-05-06 17:50:33 +0200

    implemented test container for j unit tests

--- apps/mvp/src/test/java/de/webshop/asta/mvp/MvpApplicationTests.java
+++ apps/mvp/src/test/java/de/webshop/asta/mvp/MvpApplicationTests.java
@@ -2,9 +2,19 @@ package de.webshop.asta.mvp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class MvpApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18-alpine")
+												.withInitScript("init.sql");
 
 	@Test
 	void contextLoads() {
